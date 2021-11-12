c2b074523fbead22a4218be946cf9f3aa0c49c9e
scottsak <48599109+scottsak@users.noreply.github.com>
2021-11-11 19:23:37 -0800

    challenge 13 solved

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui 
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
+let posts = [];
+
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -22,7 +24,7 @@ app.use(express.static("public"));
 
 
 app.get("/", function(req, res){
-   res.render("home",{paragraph:homeStartingContent});
+   res.render("home",{startingContent:homeStartingContent, posts:posts});
 });
 
 
@@ -39,11 +41,13 @@ app.get("/compose", function(req,res){
 });
 
 app.post("/compose", function(req,res){
-  var post = {
-    title:req.body.postTitle,
-    body:req.body.postPost
+  const post = {
+    title: req.body.postTitle,
+    body: req.body.postPost
   };
-  console.log(post);
+  posts.push(post);
+  
+  res.redirect("/");
 });
 
 
