b17b5cedf15241a93b4cc91d9983a14cb909022d
scottsak <48599109+scottsak@users.noreply.github.com>
2022-07-29 15:53:51 -0700

    removed somethings

--- app.js
+++ app.js
@@ -79,7 +79,11 @@ app.get("/contact", function(req, res){
   res.render("contact", {contactContent: contactContent});
 });
 
+let port = process.env.PORT;
+if(port == null || port==''){
+  port = 3000;
+}
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(port , function() {
+  console.log("Server started successfully");
 });
