9eb3bf30339a47cfb146c34efdaa4dde1827e96d
scottsak <48599109+scottsak@users.noreply.github.com>
2022-07-29 04:08:40 -0700

    update the blog site to work with my mongodb

--- app.js
+++ app.js
@@ -3,72 +3,79 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const _ = require('lodash');
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
-let posts = [];
-
 const app = express();
-
-mongoose.connect('mongodb://localhost:27017/blogSite');
-
-const postSchema = new mongoose.Schema{{
-  title: String,
-  body: String
-}};
-
-const Post = mongoose.model("Post", postSchema);
 
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+// mongoose.connect("mongodb+srv://scottsak:February@cluster0.wld40.mongodb.net/?retryWrites=true&w=majority/blogDB", {useNewUrlParser: true});
+mongoose.connect("'mongodb+srv://scottsak:Feb!2193803@cluster0.w3za4.mongodb.net/Blog", {useNewUrlParser: true});
+// mongoose.connect("mongodb+srv://scottsak:Feb2193803!@cluster0.oppf4xh.mongodb.net/Blog", {useNewUrlParser: true});
 
 
 
+const postSchema = {
+  title: String,
+  content: String
+};
 
-
+const Post = mongoose.model("Post", postSchema);
 
 app.get("/", function(req, res){
-   res.render("home",{startingContent:homeStartingContent, posts:posts});
+
+  Post.find({}, function(err, posts){
+    res.render("home", {
+      startingContent: homeStartingContent,
+      posts: posts
+      });
+  });
 });
 
-
-app.get("/about", function(req, res){
-    res.render("about", {aboutInfo:aboutContent});
-});
-
-app.get("/contact", function(req,res){
-  res.render("contact", {contactInfo: contactContent});
-});
-
-app.get("/compose", function(req,res){
+app.get("/compose", function(req, res){
   res.render("compose");
 });
 
-app.post("/compose", function(req,res){
-  const post = {
+app.post("/compose", function(req, res){
+  const post = new Post({
     title: req.body.postTitle,
-    body: req.body.postPost
-  };
-  posts.push(post);
+    content: req.body.postBody
+  });
 
-  res.redirect("/");
-});
 
-app.get("/posts/:postName", function(req,res){
-  var goal;
-  posts.forEach(function(post){
-    if(_.lowerCase(post.title)===_.lowerCase(req.params.postName)){
-      goal = post;
+  post.save(function(err){
+    if (!err){
+        res.redirect("/");
     }
   });
-  res.render("post", {postInfo:goal});
+});
+
+app.get("/posts/:postId", function(req, res){
+
+const requestedPostId = req.params.postId;
+
+  Post.findOne({_id: requestedPostId}, function(err, post){
+    res.render("post", {
+      title: post.title,
+      content: post.content
+    });
+  });
+
+});
+
+app.get("/about", function(req, res){
+  res.render("about", {aboutContent: aboutContent});
+});
+
+app.get("/contact", function(req, res){
+  res.render("contact", {contactContent: contactContent});
 });
 
 
